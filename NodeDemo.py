528d1882f36690ac29c802cabb61490a500fc1bf
Fish329 <268188@burlcoschools.org>
2025-04-09 12:37:54 -0400

    Update NodeDemo.py

--- NodeDemo.py
+++ NodeDemo.py
@@ -1,36 +1,37 @@
 class node:
-    def __init__(self,data,left=None,right=None):
+    def __init__(self,data,left=None,right=None,center=None):
         self.data=data
         self.left=left
         self.right=right
+        self.center=center
     
-    def insert(self,data,pos):
+    def insert(self,data,pos): #Add a left, right, or center child.
         if pos=="L":
             self.left=node(data)
         elif pos=="R":
             self.right=node(data)
+        elif pos=="C":
+            self.center=node(data)
     
-    def traverse(self,pos):
-        cur=0
-        if pos=="L":
-            cur=self.left
-        elif pos=="R":
-            cur=self.right
-        return cur
-    
-    def display(self):
+    def display(self): #Print own data, run the same code for left child, center child, and right child, if they have them
         print(self.data)
         if self.left:
             print("L: ",end="")
             self.left.display()
+        if self.center:
+            print("C: ",end="")
+            self.center.display()
         if self.right:
             print("R: ",end="")
             self.right.display()
+        print("-BT-") #Represents a backtrack
+    
        
 
-root=node("Door")
+root=node("Door") #create tree
 root.insert("Foyer","L")
 root.insert("Kitchen","R")
+root.insert("Living Room","C")
 root.left.insert("Den","L")
 root.left.insert("Bedrooms","R")
 root.right.insert("Dining Room","L")
