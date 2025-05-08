eb3a3f10c62b0e547672816235973f6d7a7f8516
Fish329 <268188@burlcoschools.org>
2025-05-08 10:16:43 -0400

    Update maze-solve.py

--- maze-solve.py
+++ maze-solve.py
@@ -1,127 +1,181 @@
 class node:
-    def __init__(self,x,y,left=None,right=None,center=None,depth=0,parent=None):
+    def __init__(self,x,y,pos=None,le=None,ri=None,ce=None,depth=0):
         self.x=x
         self.y=y
-        self.left=left
-        self.right=right
-        self.center=center
+        self.le=le
+        self.ri=ri
+        self.ce=ce
         self.depth=depth
-        self.parent=parent
+        self.pos=pos
     
-    def insert(self,x,y,pos): #Add a left, right, or center child.
-        if pos=="L":
-            self.left=node(x,y,depth=self.depth+1,parent=self)
-        elif pos=="R":
-            self.right=node(x,y,depth=self.depth+1,parent=self)
-        elif pos=="C":
-            self.center=node(x,y,depth=self.depth+1,parent=self)
-
-    def trav(self,iterations,pos): #Traverse, to marginally shorten the commands
-        recurse=self
-        for i in range(iterations):
-            if pos=="L":
-                recurse=recurse.left
-            elif pos=="C":
-                recurse=recurse.center
-            elif pos=="R":
-                recurse=recurse.right
-        return recurse
-
-root=node(0,0) #0,0
-root.insert(0,1,"L") #0,0 to 0,1
-root.left.insert(0,2,"L") #0,1 to 0,2
-root.left.left.insert(0,3,"L") #0,2 to 0,3
-root.left.left.left.insert(0,4,"L") #0,3 to 0,4
-root.left.left.left.left.insert(1,4,"L") #0,4 to 1,4
-root.trav(5,"L").insert(1,5,"L") #1,4 to 1,5
-root.trav(5,"L").insert(1,3,"R") #1,4 to 1,3
-root.trav(6,"L").insert(1,6,"L") #1,5 to 1,6
-root.trav(6,"L").insert(0,5,"R") #1,5 to 0,5
-root.trav(5,"L").right.insert(1,2,"L") #1,3 to 1,2
-root.trav(5,"L").right.insert(2,3,"R") #1,3 to 2,3
-root.trav(7,"L").insert(2,6,"L") #1,6 to 2,6
-root.trav(6,"L").right.insert(0,6,"L") #0,5 to 0,6
-root.trav(6,"L").right.left.insert(1,1,"L")#1,2 to 1,1
-root.trav(5,"L").right.right.insert(2,4,"L")#2,3 to 2,4
-root.trav(5,"L").right.right.insert(2,2,"R")#2,3 to 2,2
-root.trav(8,"L").insert(3,6,"L") #2,6 to 3,6
-root.trav(8,"L").insert(2,7,"R") #2,6 to 2,7
-root.trav(6,"L").right.left.insert(0,7,"L") #0,6 to 0,7
-root.trav(6,"L").right.left.left.insert(1,0,"L") #1,1 to 1,0
-root.trav(6,"L").right.left.left.insert(2,1,"R") #1,1 to 2,1
-root.trav(5,"L").right.right.left.insert(2,5,"L") #2,4 to 2,5
-root.trav(5,"L").trav(3,"R").insert(3,2,"L")#2,2 to 3,2
-root.trav(9,"L").insert(3,5,"L")#3,6 to 3,5
-root.trav(8,"L").right.insert(3,7,"L") #2,7 to 3,7
-root.trav(6,"L").right.left.left.insert(1,7,"L")#0,7 to 1,7
-root.trav(6,"L").right.trav(3,"L").insert(2,0,"L")#1,0 to 2,0
-root.trav(6,"L").right.left.left.right.insert(3,1,"L")#2,1 to 3,1
-root.trav(5,"L").trav(3,"R").left.insert(4,2,"L")#3,2 to 4,2
-root.trav(8,"L").right.left.insert(4,7,"L")#3,7 to 4,7
-root.trav(6,"L").right.trav(4,"L").insert(3,0,"L")#2,0 to 3,0
-root.trav(6,"L").right.left.left.right.left.insert(4,1,"L")#3,1 to 4,1
-root.trav(5,"L").trav(3,"R").left.left.insert(5,2,"L")#4,2 to 5,2
-root.trav(8,"L").right.left.left.insert(5,7,"L")#4,7 to 5,7
-root.trav(6,"L").right.left.left.right.left.left.insert(5,1,"L")#4,1 to 5,1
-root.trav(6,"L").right.left.left.right.left.left.insert(4,0,"R")#4,1 to 4,0
-root.trav(5,"L").trav(3,"R").trav(3,"L").insert(5,3,"L")#5,2 to 5,3
-root.trav(5,"L").trav(3,"R").trav(3,"L").insert(6,2,"R")#5,2 to 6,2
-root.trav(8,"L").right.trav(3,"L").insert(5,6,"L")#5,7 to 5,6
-root.trav(8,"L").right.trav(3,"L").insert(6,7,"R")#5,7 to 6,7
-root.trav(6,"L").right.left.left.right.trav(3,"L").insert(5,0,"L")#5,1 to 5,0
-root.trav(5,"L").trav(3,"R").trav(4,"L").insert(5,4,"L")#5,3 to 5,4
-root.trav(8,"L").right.trav(3,"L").right.insert(7,2,"L")#6,2 to 7,2
-root.trav(8,"L").right.trav(3,"L").right.insert(6,3,"R")#6,2 to 6,3
-root.trav(8,"L").right.trav(3,"L").right.insert(7,7,"L")#6,7 to 7,7
-root.trav(6,"L").right.left.left.right.trav(4,"L").insert(6,0,"L")#5,0 to 6,0
-root.trav(8,"L").right.trav(3,"L").right.left.insert(7,3,"L")#7,2 to 7,3
-root.trav(8,"L").right.trav(3,"L").right.left.insert(7,1,"R")#7,2 to 7,1
-root.trav(8,"L").right.trav(3,"L").right.right.insert(6,4,"L")#6,3 to 6,4
-root.trav(8,"L").right.trav(3,"L").right.left.insert(7,6,"L")#7,7 to 7,6
-root.trav(6,"L").right.left.left.right.trav(5,"L").insert(6,1,"L")#6,0 to 6,1
-root.trav(8,"L").right.trav(3,"L").right.left.left.insert(7,4,"L")#7,3 to 7,4
-root.trav(8,"L").right.trav(3,"L").right.left.right.insert(7,0,"L")#7,1 to 7,0
-root.trav(8,"L").right.trav(3,"L").right.right.left.insert(6,5,"L")#6,4 to 6,5
-root.trav(8,"L").right.trav(3,"L").right.left.left.insert(6,6,"L")#7,6 to 6,6
-move=root.trav(5,"L").trav(3,"R").trav(3,"L").right
-root.trav(6,"L").right.left.left.right.trav(5,"L").left=move #6,1 to 6,2
-root.trav(8,"L").right.trav(3,"L").right.right.left.left.insert(5,5,"L")#6,5 to 5,5
-root.trav(8,"L").right.trav(3,"L").right.right.left.left.insert(7,5,"R")#6,5 to 7,5
-move=root.trav(8,"L").right.trav(3,"L").right.right.left.left
-root.trav(8,"L").right.trav(3,"L").right.trav(3,"L").left=move #6,6 to 6,5
-root.trav(8,"L").right.trav(3,"L").right.right.trav(3,"L").insert(4,5,"L") #5,5 to 4,5
-root.trav(8,"L").right.trav(3,"L").right.right.trav(4,"L").insert(4,6,"L") #4,5 to 4,6
-root.trav(8,"L").right.trav(3,"L").right.right.trav(4,"L").insert(4,4,"L") #4,5 to 4,4 (END)
-
-#The maze, crushed down into one string
-mazepos="+---+---+---+---+---+---+---+---+|       |                       |+   +---+   +---+---+   +---+   +|   |           |   |   |       |+   +   +---+   +   +---+   +---+|       |   |   |               |+---+   +   +---+   +---+   +---+|       |   |       |   |   |   |+   +   +   +       +   +   +   +|   |       |       |   |   |   |+   +   +   +---+---+   +   +   +|   |   |                       |+   +   +---+---+---+---+   +   +|   |                   |   |   |+   +   +---+---+   +   +   +   +|   |           |   |       |   |+---+---+---+---+---+---+---+---+"
-#maze width: 33 chars
-#height: 17
-curpos=497
-curNode=root
-prevNode=0
-# up 1 space: -66
-# down 1 space: +66
-# left 1 space: -4
-# right 1 space: +4
-def drawmaze(curpos,x,y):
-    itr=0 #counter
-    for i in range(x):
-        curpos=curpos-4
-    for i in range(y):
-        curpos=curpos-66
-    for i in range(17): #print 17 lines
-        for j in range(33): #print 33 chars per line
-            if curpos==itr: #if the mouse's position is equal to the current tile's position, replace it
-                print("@",end="")
+    def insL(self,wnode): #insert a node at the left position
+            self.le=wnode
+            wnode.depth=self.depth+1
+            if wnode.pos!=None:
+                wnode.pos="X"
             else:
-                print (list(mazepos)[itr],end="") #otherwise print the tile
-            itr=itr+1
-        print("")
-    print("available moves:")
-    if curNode.left:
-        print("Left (",curNode.left.x,",",curNode.left.y,")",sep="")
-    if curNode.center:
-        print("Right (",curNode.center.x,",",curNode.center.y,")",sep="")
-        #WIP
-drawmaze(curpos,0,0)
+                wnode.pos="L"
+    def insR(self,wnode): #insert at the right position
+            self.ri=wnode
+            wnode.depth=self.depth+1
+            if wnode.pos!=None:
+                wnode.pos="X"
+            else:
+                wnode.pos="R"
+    def insC(self,wnode): #insert at the center position
+            self.ce=wnode
+            wnode.depth=self.depth+1
+            if wnode.pos!=None:
+                wnode.pos="X"
+            else:
+                wnode.pos="C"
+    
+    def display(self,parent): #Print own data, run the same code for left child, center child, and right child, if they have them
+        if self.depth>0:
+            for i in range(self.depth):
+                print("-",end="")
+            print("(",self.x,",",self.y,") ","(",self.pos,")"," [",parent.x,",",parent.y,"]",sep="")
+        else:
+            print("(",self.x,",",self.y,")",sep="")
+        if self.le:
+            self.le.depth=self.depth+1
+            self.le.display(self)
+        if self.ce:
+            self.ce.depth=self.depth+1
+            self.ce.display(self)
+        if self.ri:
+            self.ri.depth=self.depth+1
+            self.ri.display(self)
+#define nodes
+c00=node(0,0)
+c01=node(0,1)
+c02=node(0,2)
+c03=node(0,3)
+c04=node(0,4)
+c05=node(0,5)
+c06=node(0,6)
+c07=node(0,7)
+c10=node(1,0)
+c11=node(1,1)
+c12=node(1,2)
+c13=node(1,3)
+c14=node(1,4)
+c15=node(1,5)
+c16=node(1,6)
+c17=node(1,7)
+c20=node(2,0)
+c21=node(2,1)
+c22=node(2,2)
+c23=node(2,3)
+c24=node(2,4)
+c25=node(2,5)
+c26=node(2,6)
+c27=node(2,7)
+c30=node(3,0)
+c31=node(3,1)
+c32=node(3,2)
+c33=node(3,3)
+c34=node(3,4)
+c35=node(3,5)
+c36=node(3,6)
+c37=node(3,7)
+c40=node(4,0)
+c41=node(4,1)
+c42=node(4,2)
+c43=node(4,3)
+c44=node(4,4)
+c45=node(4,5)
+c46=node(4,6)
+c47=node(4,7)
+c50=node(5,0)
+c51=node(5,1)
+c52=node(5,2)
+c53=node(5,3)
+c54=node(5,4)
+c55=node(5,5)
+c56=node(5,6)
+c57=node(5,7)
+c60=node(6,0)
+c61=node(6,1)
+c62=node(6,2)
+c63=node(6,3)
+c64=node(6,4)
+c65=node(6,5)
+c66=node(6,6)
+c67=node(6,7)
+c70=node(7,0)
+c71=node(7,1)
+c72=node(7,2)
+c73=node(7,3)
+c74=node(7,4)
+c75=node(7,5)
+c76=node(7,6)
+c77=node(7,7)
+#connect nodes
+c00.insC(c01)
+c01.insC(c02)
+c02.insC(c03)
+c03.insC(c04)
+c04.insL(c14)
+c14.insL(c15)
+c14.insR(c13)
+c15.insL(c05)
+c15.insC(c16)
+c13.insL(c23)
+c13.insC(c12)
+c05.insC(c06)
+c16.insL(c26)
+c23.insL(c24)
+c23.insR(c22)
+c12.insC(c11)
+c06.insC(c07)
+c26.insL(c27)
+c26.insC(c36)
+c24.insC(c25)
+c22.insL(c32)
+c11.insC(c10)
+c11.insL(c21)
+c07.insL(c17)
+c27.insL(c37)
+c36.insR(c35)
+c32.insC(c42)
+c10.insL(c20)
+c21.insC(c31)
+c37.insC(c47)
+c42.insC(c52)
+c20.insC(c30)
+c31.insC(c41)
+c47.insC(c57)
+c52.insL(c53)
+c52.insC(c62)
+c41.insL(c40)
+c41.insC(c51)
+c57.insL(c56)
+c57.insC(c67)
+c53.insC(c54)
+c62.insC(c72)
+c62.insL(c63)
+c51.insR(c50)
+c67.insC(c77)
+c72.insL(c73)
+c72.insR(c71)
+c63.insC(c64)
+c50.insL(c60)
+c77.insL(c76)
+c73.insC(c74)
+c71.insC(c70)
+c64.insC(c65)
+c60.insL(c61)
+c76.insR(c66)
+c65.insR(c75)
+c65.insL(c55)
+c61.insC(c62)
+c66.insL(c65)
+c55.insC(c45)
+c45.insR(c46)
+c45.insL(c44)
+#display root
+myNode=c00
+myNode.display(None)
+print("\"X\" indicates a crossroads, where different paths converge")
+print("coords in [brackets] indicates the node's parent.")
