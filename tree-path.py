99ca8a27e3e0460365983ca7055f7eb8ccefd765
Fish329 <268188@burlcoschools.org>
2025-06-09 09:19:57 -0400

    Update tree-path.py

--- tree-path.py
+++ tree-path.py
@@ -1,75 +1,52 @@
-#this is wip
 import random
-#define nodes
 class node:
-    def __init__(self,data,depth=0,children=0,left=None,center=None,right=None,parent=None):
-        self.data=data
-        self.depth=depth
-        self.left=left
-        self.right=right
-        self.center=center
-        self.parent=parent
-        self.children=children
-    #define functions
+    def __init__(self,data,parent=None,left=None,center=None,right=None,depth=0,endExist=False,end=False):
+        self.left=left #left child
+        self.center=center #center child
+        self.right=right #right child
+        self.depth=depth #node depth
+        self.parent=parent #this node's parent
+        self.endExist=endExist #flag for if the end node has been created
+        self.end=end #flag for if this node is the end node
+        self.data=data #Node's data
+        if self.parent!=None:
+            self.depth=self.parent.depth+1
     def spawn(self):
-        pool=[0,1,2]
-        for i in range(2):
-            pick=random.choice(pool)
-            if i>0:
-                if random.randrange(0,2)==0:
-                    pool.remove(pick)
-                    continue
-            if pick==0:
-                self.insL()
-            elif pick==1:
-                self.insC()
-            elif pick==2:
-                self.insR()
-            pool.remove(pick)
-            
-    def insL(self): 
-        self.left=node(random.randrange(0,100),depth=self.depth+1,parent=self)
-        self.children+=1
-        if self.depth<2:
-            if self.children>1:
-                if random.randrange(0,2)==0:
-                    return
-            self.left.spawn()
-
-    def insC(self):
-        self.center=node(random.randrange(0,100),depth=self.depth+1,parent=self)
-        self.children+=1
-        if self.depth<2:
-            if self.children>1:
-                if random.randrange(0,2)==0:
-                    return
-            self.center.spawn()
-            
-    def insR(self):
-        self.right=node(random.randrange(0,100),depth=self.depth+1,parent=self)
-        self.children+=1
-        if self.depth<2:
-            if self.children>1:
-                if random.randrange(0,2)==0:
-                    return
-            self.right.spawn()
-
+        if self.depth<3:
+            roll=random.randrange(1,4)
+            for i in range(roll-1):
+                pool=["L","C","R"]
+                if self.left:
+                    pool.remove("L")
+                if self.center:
+                    pool.remove("C")
+                if self.right:
+                    pool.remove("R")
+                pick=random.choice(pool)
+                if pick=="L":
+                    self.left=node(random.randrange(0,100),self)
+                    self.left.spawn()
+                if pick=="C":
+                    self.center=node(random.randrange(0,100),self)
+                    self.center.spawn()
+                if pick=="R":
+                    self.right=node(random.randrange(0,100),self)
+                    self.right.spawn
     def display(self):
-        for i in range(self.depth):
+        for i in (range(self.depth)):
             print("-",end="")
-        print(" ",self.data," (",self.depth,")",sep="")
+        print(self.data)
         if self.left:
-            print("L: ",end="")
+            print("L:",end="")
             self.left.display()
         if self.center:
-            print("C: ",end="")
+            print("C:",end="")
             self.center.display()
         if self.right:
-            print("R: ",end="")
+            print("R:",end="")
             self.right.display()
-        
+            
 root=node(random.randrange(0,100))
 root.spawn()
-print("S:",end="")
 root.display()
 #this is wip
