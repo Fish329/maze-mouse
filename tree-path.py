a5b9293d67e89fa7f180426010569c4917b1924d
Fish329 <268188@burlcoschools.org>
2025-06-10 08:46:52 -0400

    Update tree-path.py

--- tree-path.py
+++ tree-path.py
@@ -11,10 +11,11 @@ class node:
         self.data=data #Node's data
         if self.parent!=None:
             self.depth=self.parent.depth+1
+            
     def spawn(self):
         if self.depth<3:
             roll=random.randrange(1,4)
-            for i in range(roll-1):
+            for i in range(roll):
                 pool=["L","C","R"]
                 if self.left:
                     pool.remove("L")
@@ -24,18 +25,27 @@ class node:
                     pool.remove("R")
                 pick=random.choice(pool)
                 if pick=="L":
-                    self.left=node(random.randrange(0,100),self)
+                    self.left=node(random.randrange(0,100),self,endExist=self.endExist)
                     self.left.spawn()
                 if pick=="C":
-                    self.center=node(random.randrange(0,100),self)
+                    self.center=node(random.randrange(0,100),self,endExist=self.endExist)
                     self.center.spawn()
                 if pick=="R":
-                    self.right=node(random.randrange(0,100),self)
-                    self.right.spawn
+                    self.right=node(random.randrange(0,100),self,endExist=self.endExist)
+                    self.right.spawn()
+        else:
+            if self.endExist==False:
+                self.end=True
+                self.tellUp()
+        
     def display(self):
         for i in (range(self.depth)):
             print("-",end="")
-        print(self.data)
+        print(self.data,end="")
+        if self.end==True:
+            print(" FINISH")
+        else:
+            print("")
         if self.left:
             print("L:",end="")
             self.left.display()
@@ -46,6 +56,22 @@ class node:
             print("R:",end="")
             self.right.display()
             
+    def tellUp(self):
+        self.endExist=True
+        if self.parent and self.parent.endExist==False:
+            self.parent.tellDown()
+    
+    def tellDown(self):
+        self.endExist=True
+        if self.left!=None and self.left.endExist==False:
+            self.left.endExist=True
+        if self.center!=None and self.center.endExist==False:
+            self.center.endExist=True
+        if self.right!=None and self.right.endExist==False:
+            self.right.endExist=True
+        if self.parent!=None and self.parent.endExist==False:
+            self.parent.tellDown()
+                
 root=node(random.randrange(0,100))
 root.spawn()
 root.display()
